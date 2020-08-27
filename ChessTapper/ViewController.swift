d9dacad408bbe23645f09caef3334ae015cf3070
Emil Baehr <emil.baehr@gmail.com>
2020-08-27 13:52:09 +0200

    Initial Commit

---
+++ ChessTapper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChessTapper
+//
+//  Created by Emil Malthe Bæhr Christensen on 27/08/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
