55f89013a060daa22ad57df32a0ffbb73d6bc6dc
Emil Baehr <emil.baehr@gmail.com>
2020-09-17 21:00:10 +0200

    Play with gesture recognisers for tapping the clocks

--- ChessTapper/ClockViewController.swift
+++ ChessTapper/ClockViewController.swift
@@ -11,44 +11,51 @@ class ClockViewController: UIViewController {
 
     var timeKeeper: TimeKeeper?
     
+    let clock1 = UIView()
+    let clock2 = UIView()
+    
     let label1 = UILabel(frame: .zero)
     let label2 = UILabel(frame: .zero)
-    let button = UIButton(type: .system)
-    
-    var observers = [NSKeyValueObservation]()
+//    let button = UIButton(type: .system)
         
+    var observers = [NSKeyValueObservation]()
+    
     override func loadView() {
+
         let rootView = UIView()
         rootView.backgroundColor = .systemPink
         
-        let clock1 = UIView()
         clock1.backgroundColor = .systemGray
-        
-        let clock2 = UIView()
         clock2.backgroundColor = .systemOrange
-        
-        rootView.addSubview(clock1)
-        rootView.addSubview(clock2)
-        
         clock1.translatesAutoresizingMaskIntoConstraints = false
         clock2.translatesAutoresizingMaskIntoConstraints = false
         
         label1.text = TimeInterval(300).stringFromTimeInterval()
         label1.textAlignment = .center
-        clock1.addSubview(label1)
         label1.translatesAutoresizingMaskIntoConstraints = false
         
         label2.text = TimeInterval(300).stringFromTimeInterval()
         label2.textAlignment = .center
-        clock2.addSubview(label2)
         label2.translatesAutoresizingMaskIntoConstraints = false
         
-        button.setTitle("Switch", for: .normal)
-        clock1.addSubview(button)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.addTarget(self, action: #selector(switchTurn), for: .touchUpInside)
-
-
+        clock1.addSubview(label1)
+        clock2.addSubview(label2)
+        
+        rootView.addSubview(clock1)
+        rootView.addSubview(clock2)
+        
+        let clock1Tap = UITapGestureRecognizer(target: self, action:  #selector(switchTurn(_:)))
+        let clock2Tap = UITapGestureRecognizer(target: self, action:  #selector(switchTurn(_:)))
+        clock1.addGestureRecognizer(clock1Tap)
+        clock2.addGestureRecognizer(clock2Tap)
+        clock1.isUserInteractionEnabled = true
+        clock2.isUserInteractionEnabled = true
+        
+//        button.setTitle("Switch", for: .normal)
+//        clock1.addSubview(button)
+//        button.translatesAutoresizingMaskIntoConstraints = false
+//        button.addTarget(self, action: #selector(switchTurn), for: .touchUpInside)
+        
         let constraints = [
             clock1.widthAnchor.constraint(equalTo: rootView.widthAnchor),
             clock1.heightAnchor.constraint(equalTo: rootView.safeAreaLayoutGuide.heightAnchor, multiplier: 0.5),
@@ -69,8 +76,8 @@ class ClockViewController: UIViewController {
             label2.centerXAnchor.constraint(equalTo: clock2.centerXAnchor),
             label2.centerYAnchor.constraint(equalTo: clock2.centerYAnchor),
             
-            button.centerXAnchor.constraint(equalTo: clock1.centerXAnchor),
-            button.topAnchor.constraint(equalTo: label1.bottomAnchor)
+//            button.centerXAnchor.constraint(equalTo: clock1.centerXAnchor),
+//            button.topAnchor.constraint(equalTo: label1.bottomAnchor)
         ]
         NSLayoutConstraint.activate(labelConstraints)
         
@@ -94,7 +101,8 @@ class ClockViewController: UIViewController {
         ]
     }
     
-    @objc func switchTurn() {
+    @objc func switchTurn(_ sender: UITapGestureRecognizer) {
+        print("Tapped!")
         timeKeeper?.switchTurn()
     }
 
