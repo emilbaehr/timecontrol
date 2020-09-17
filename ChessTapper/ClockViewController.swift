e2ac42c3855162043a326f3baf173a6023a99d2e
Emil Baehr <emil.baehr@gmail.com>
2020-09-17 16:50:16 +0200

    Added observers to observe TimeInterval objects of model

--- ChessTapper/ClockViewController.swift
+++ ChessTapper/ClockViewController.swift
@@ -9,12 +9,20 @@ import UIKit
 
 class ClockViewController: UIViewController {
 
+    var timeKeeper: TimeKeeper?
+    
+    let label1 = UILabel(frame: .zero)
+    let label2 = UILabel(frame: .zero)
+    let button = UIButton(type: .system)
+    
+    var observers = [NSKeyValueObservation]()
+        
     override func loadView() {
         let rootView = UIView()
         rootView.backgroundColor = .systemPink
         
         let clock1 = UIView()
-        clock1.backgroundColor = .systemBlue
+        clock1.backgroundColor = .systemGray
         
         let clock2 = UIView()
         clock2.backgroundColor = .systemOrange
@@ -25,17 +33,21 @@ class ClockViewController: UIViewController {
         clock1.translatesAutoresizingMaskIntoConstraints = false
         clock2.translatesAutoresizingMaskIntoConstraints = false
         
-        let label1 = UILabel(frame: .zero)
-        label1.text = "0:00"
+        label1.text = TimeInterval(300).stringFromTimeInterval()
         label1.textAlignment = .center
         clock1.addSubview(label1)
         label1.translatesAutoresizingMaskIntoConstraints = false
         
-        let label2 = UILabel(frame: .zero)
-        label2.text = "0:00"
+        label2.text = TimeInterval(300).stringFromTimeInterval()
         label2.textAlignment = .center
         clock2.addSubview(label2)
         label2.translatesAutoresizingMaskIntoConstraints = false
+        
+        button.setTitle("Switch", for: .normal)
+        clock1.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(switchTurn), for: .touchUpInside)
+
 
         let constraints = [
             clock1.widthAnchor.constraint(equalTo: rootView.widthAnchor),
@@ -55,17 +67,35 @@ class ClockViewController: UIViewController {
             
             label2.widthAnchor.constraint(equalTo: clock2.widthAnchor),
             label2.centerXAnchor.constraint(equalTo: clock2.centerXAnchor),
-            label2.centerYAnchor.constraint(equalTo: clock2.centerYAnchor)
+            label2.centerYAnchor.constraint(equalTo: clock2.centerYAnchor),
+            
+            button.centerXAnchor.constraint(equalTo: clock1.centerXAnchor),
+            button.topAnchor.constraint(equalTo: label1.bottomAnchor)
         ]
-        
         NSLayoutConstraint.activate(labelConstraints)
         
         self.view = rootView
+    }
+    
+    override func viewDidLoad() {
         
-        let timeKeeper1 = TimeKeeper(time: 60, gameType: .suddenDeath)
-        
-        timeKeeper1.startTime(color: .white)
+        timeKeeper = TimeKeeper(time: 300, gameType: .suddenDeath)
+
+        // Starting time should be done by user.
+        timeKeeper?.startTime()
+    
+        observers = [
+            timeKeeper!.observe(\.whiteTime, options: .new) { (tk, change) in
+                self.label1.text = tk.whiteTime.stringFromTimeInterval()
+            },
+            timeKeeper!.observe(\.blackTime, options: .new) { (tk, change) in
+                self.label2.text = tk.blackTime.stringFromTimeInterval()
+            }
+        ]
+    }
+    
+    @objc func switchTurn() {
+        timeKeeper?.switchTurn()
     }
 
 }
-
--- ChessTapper/TimeKeeper.swift
+++ ChessTapper/TimeKeeper.swift
@@ -7,11 +7,13 @@
 
 import Foundation
 
-class TimeKeeper {
+@objc class TimeKeeper: NSObject {
 
     private var timer: Timer?
-    var whiteTime: TimeInterval
-    var blackTime: TimeInterval
+    
+    @objc dynamic var whiteTime: TimeInterval
+    @objc dynamic var blackTime: TimeInterval
+    
     var playerTurn: Color
     var gameType: GameType
     
@@ -32,24 +34,65 @@ class TimeKeeper {
     init(time seconds: TimeInterval, gameType: GameType) {
         self.whiteTime = seconds
         self.blackTime = seconds
+        
+        // Always begin as white.
         self.playerTurn = .white
+        
         self.gameType = gameType
     }
     
-    func startTime(color: Color) {
-        
+    func startTime() {
+        self.timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
     }
     
-    func stopTime(color: Color) {
-        
+    func stopTime() {
+        timer?.invalidate()
     }
     
     func switchTurn() {
         
-    }
-    
-    func updateTime() {
+        // Set playerTurn.
+        if (playerTurn == .white) {
+            playerTurn = .black
+        } else {
+            playerTurn = .white
+        }
         
+        // Do Fischer increment here.
     }
     
+    @objc func updateTime() {
+        
+        switch playerTurn {
+            case .white:
+                whiteTime -= 1.0
+            case .black:
+                blackTime -= 1.0
+        }
+        
+        if (whiteTime == 0 || blackTime == 0) {
+            stopTime()
+        }
+        
+        print("White: \(whiteTime)")
+        print("Black: \(blackTime)")
+    }
+    
+}
+
+extension TimeInterval {
+
+    func stringFromTimeInterval() -> String {
+
+        // Get the time of the TimeInterval object, in seconds.
+        let time = NSInteger(self)
+
+        // Do time conversions.
+        let seconds = time % 60
+        let minutes = (time / 60) % 60
+//        let hours = (time / 3600)
+
+        // Format as String.
+        return String(format: "%0.2d:%0.2d", minutes, seconds)
+    }
 }
