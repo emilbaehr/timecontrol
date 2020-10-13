643c1676ea6a771e9206416c102b9f79649fb6f7
Emil Baehr <emil.baehr@gmail.com>
2020-10-13 17:10:07 +0200

    Round TimeInterval up before formatting in TimeIntervalFormatter

--- ChessTapper/TimeIntervalFormatter.swift
+++ ChessTapper/TimeIntervalFormatter.swift
@@ -9,18 +9,30 @@ import Foundation
 
 public class TimeIntervalFormatter: DateComponentsFormatter {
     
+    private var numberFormatter: NumberFormatter?
+    
     public override init() {
         super.init()
         self.unitsStyle = .positional
         self.allowedUnits = [.hour, .minute, .second]
+        self.zeroFormattingBehavior = [.dropLeading, .pad]
+        
+        self.numberFormatter = NumberFormatter()
+        self.numberFormatter?.minimumFractionDigits = 0
+        self.numberFormatter?.maximumFractionDigits = 2
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
     
-//    public override func string(from ti: TimeInterval) -> String? {
-//        return super.string(from: ti)
-//    }
+    public override func string(from ti: TimeInterval) -> String? {
+        
+        let seconds = ti / 60 / 60
+        let milliseconds = numberFormatter?.string(from: NSNumber(value: seconds))
+        
+        return super.string(from: ti.rounded(.up))
+//        return super.string(from: ti)! + Locale.current.decimalSeparator! + milliseconds!
+    }
     
 }
