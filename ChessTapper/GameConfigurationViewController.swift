950c3318c86ec2f492fd8e70ea4b1d169a61b0d3
Emil Baehr <emil.baehr@gmail.com>
2020-10-18 21:13:18 +0200

    Add a navigation bar to Time Control settings

--- ChessTapper/GameConfigurationViewController.swift
+++ ChessTapper/GameConfigurationViewController.swift
@@ -8,10 +8,13 @@
 import Foundation
 import UIKit
 
-class GameConfigurationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class GameConfigurationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UINavigationBarDelegate {
     
     weak var delegate: GameConfigurationDelegate?
     var tableView = UITableView(frame: .zero, style: .insetGrouped)
+    var navBar = UINavigationBar(frame: .zero)
+    
+    var selectedTimeControl: TimeControl?
     
     let timeControls: Array<TimeControl> = [
         SuddenDeath(of: 300),
@@ -25,17 +28,32 @@ class GameConfigurationViewController: UIViewController, UITableViewDelegate, UI
     override func loadView() {
         super.loadView()
         
-        view.addSubview(tableView)
+        // Add Nav Bar.
+        self.view.addSubview(navBar)
+        navigationItem.title = "Time Control"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(done))
+        navBar.setItems ([navigationItem], animated: false)
         
+        navBar.delegate = self
+        navBar.translatesAutoresizingMaskIntoConstraints = false
+        navBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        navBar.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        navBar.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+                
+        // Add Table View.
+        self.view.addSubview(tableView)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        tableView.contentInset = UIEdgeInsets(top: 44, left: 0,bottom: 0, right: 0)
         
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        self.view.bringSubviewToFront(navBar)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -48,11 +66,19 @@ class GameConfigurationViewController: UIViewController, UITableViewDelegate, UI
         return cell
     }
     
+    @objc func done() {
+        
+        if let timeControl = selectedTimeControl {
+            let timekeeper = Timekeeper(whitePlayer: timeControl, blackPlayer: timeControl)
+            delegate?.finishedConfiguringTimekeeper(timekeeper)
+        }
+        
+        self.dismiss(animated: true)
+    }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let timeControl = timeControls[indexPath.row]
-        let timekeeper = Timekeeper(whitePlayer: timeControl, blackPlayer: timeControl)
+        selectedTimeControl = timeControls[indexPath.row]
         print("Selected row \(indexPath.row).")
-        delegate?.finishedConfiguringTimekeeper(timekeeper)
     }
     
 }
