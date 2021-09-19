8345194f665e69f1e8e739452cb443cc387e8ca1
Emil Baehr <emil.baehr@gmail.com>
2021-09-19 22:32:07 +0200

    Set up basic test setup for testing different Timekeeping modes

--- Tests/TimeControlTests.swift
+++ Tests/TimeControlTests.swift
@@ -17,43 +17,58 @@ class TimeControlTests: XCTestCase {
         let duration: TimeInterval
         let increment: TimeInterval? = nil
         let delay: TimeInterval? = nil
+        let remaining: TimeInterval
     }
     
     func testSuddenDeathTimekeeping() {
         
         let seconds = TimeInterval(60)
+        
         let timeControl = TimeControl(of: seconds)
         let timekeeper = Timekeeper(playerOne: timeControl, playerTwo: timeControl)
         let expectation = XCTestExpectation(description: "Timekeeper stops when time is up for either player.")
         
         let moves = [
-            Move(player: timekeeper.playerOne, move: 1, duration: 2),
-            Move(player: timekeeper.playerTwo, move: 1, duration: 2)
+            Move(player: timekeeper.playerOne, move: 1, duration: 2, remaining: 58),
+            Move(player: timekeeper.playerTwo, move: 1, duration: 2, remaining: 58),
+            Move(player: timekeeper.playerOne, move: 2, duration: 1, remaining: 57),
+            Move(player: timekeeper.playerTwo, move: 2, duration: 1, remaining: 57),
+            Move(player: timekeeper.playerOne, move: 3, duration: 0.5, remaining: 56.5),
+            Move(player: timekeeper.playerTwo, move: 3, duration: 0.5, remaining: 56.5)
         ]
         
         XCTAssertEqual(timekeeper.playerOne.remainingTime, seconds)
+        XCTAssertEqual(timekeeper.playerTwo.remainingTime, seconds)
+        
+        var totalTime: TimeInterval = 0
         
         do {
+            
             try timekeeper.start()
             
             for move in moves {
                 
-            }
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                XCTAssertEqual(timekeeper.playerOne.remainingTime, 58, accuracy: 0.1)
-                
-                print(timekeeper.playerOne.remainingTime)
-                
-                expectation.fulfill()
+                DispatchQueue.main.asyncAfter(deadline: .now() + move.duration + totalTime) {
+                    
+                    XCTAssertEqual(move.player.remainingTime, move.remaining, accuracy: 0.1)
+                    print("Remaining: \(move.player.remainingTime)")
+                    print("Movecount: \(move.move)")
+                    try? timekeeper.switchTurn()
+                }
+                                
+                totalTime += move.duration
             }
             
+            print("All moves scheduled.")
+            
+            // If all moves were timekept correctly, fulfill the expectation.
+//            expectation.fulfill()
+            
         } catch {
-            print(error.localizedDescription)
+            print("Error testing timekeeping.")
         }
         
-        wait(for: [expectation], timeout: 60 + 1)
-        
-        XCTAssertEqual(2, 2)
+        wait(for: [expectation], timeout: seconds + 1)
     }
     
     
